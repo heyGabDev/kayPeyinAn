665591a9f549d2777261aed15411a271f9954a03
heyGabDev <heygab.consulting@gmail.com>
2025-10-02 15:52:39 +0200

    <feat>(<Products>):<Add pages -> Create display, paginator & filter>

--- kayPeyinAnApp/src/app/app.routes.ts
+++ kayPeyinAnApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        title: 'Nos produits',
+        loadComponent: () => import('./features/products/products-component').then(c => c.ProductsComponent)
+    }
+];
